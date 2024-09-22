71a9e6c05c72ea8e6e1dcc89258f19c453d360e5
nazex2000 <nazariowizz@gmail.com>
2024-09-22 11:27:24 +0200

    'Paginator'

--- bookAPI/views.py
+++ bookAPI/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from .models import Book, BookCategory
 from .serializers import BookSerializer, BookCategorySerializer
 from rest_framework.filters import SearchFilter
+from django.core.paginator import Paginator, EmptyPage
 
 # Create your views here.
 
@@ -24,6 +25,9 @@ class BookViewSet(generics.ListCreateAPIView):
         category = self.request.query_params.get('category', None)
         price_from = self.request.query_params.get('price_from', None)
         price_to = self.request.query_params.get('price_to', None)
+        ordering = self.request.query_params.get('ordering', None)
+        perpage = self.request.query_params.get('perpage', None)
+        page = self.request.query_params.get('page', None)
         queryset = Book.objects.all()
         if category:
             queryset = queryset.filter(category__id=category)
@@ -31,6 +35,14 @@ class BookViewSet(generics.ListCreateAPIView):
             queryset = queryset.filter(price__gte=price_from)
         if price_to:
             queryset = queryset.filter(price__lte=price_to)
+        if ordering:
+            ordering_fieds = ordering.split(',')
+            queryset = queryset.order_by(*ordering_fieds)
+        paginator = Paginator(queryset, perpage)
+        try:
+            queryset = paginator.page(number=page)
+        except EmptyPage:
+            queryset = paginator.page(number=paginator.num_pages)
         return queryset
 
 class BookDetailViewSet(generics.RetrieveUpdateDestroyAPIView):
